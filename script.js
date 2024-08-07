5e138c508acd05fc3d548025bacb998492cd4df8
Francine Danielle <137731537+francinedds@users.noreply.github.com>
2024-08-07 20:10:47 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,78 @@
+const slidePage = document.querySelector(".slide-page");
+const firstNextBtn = document.querySelector(".next-btn");
+const prevBtnSec = document.querySelector(".prev-1");
+const nextBtnSec = document.querySelector(".next-1");
+const prevBtnThird = document.querySelector(".prev-2");
+const nextBtnThird = document.querySelector(".next-2");
+const prevBtnFourth = document.querySelector(".prev-3");
+const submitBtn = document.querySelector(".submit");
+const progressText = document.querySelectorAll(".step p");
+const progressCheck = document.querySelectorAll(".step .check");
+const bullet = document.querySelectorAll(".step .bullet");
+let max = 4;
+let current = 1;
+
+firstNextBtn.addEventListener('click', () => {
+    slidePage.style.marginLeft = "-25%";
+    bullet[current - 1].classList.add('active');
+    progressText[current - 1].classList.add('active');
+    progressCheck[current - 1].classList.add('active');
+    current += 1;
+});
+
+nextBtnSec.addEventListener('click', () => {
+    slidePage.style.marginLeft = "-50%";
+    bullet[current - 1].classList.add('active');
+    progressText[current - 1].classList.add('active');
+    progressCheck[current - 1].classList.add('active');
+    current += 1;
+});
+
+nextBtnThird.addEventListener('click', () => {
+    slidePage.style.marginLeft = "-75%";
+    bullet[current - 1].classList.add('active');
+    progressText[current - 1].classList.add('active');
+    progressCheck[current - 1].classList.add('active');
+    current += 1;
+});
+
+// botões de next até aqui
+
+prevBtnSec.addEventListener('click', () => {
+    slidePage.style.marginLeft = "0";
+    bullet[current - 2].classList.remove('active');
+    progressText[current - 2].classList.remove('active');
+    progressCheck[current - 2].classList.remove('active');
+    current -= 1;
+});
+
+prevBtnThird.addEventListener('click', () => {
+    slidePage.style.marginLeft = "-25%";
+    bullet[current - 2].classList.remove('active');
+    progressText[current - 2].classList.remove('active');
+    progressCheck[current - 2].classList.remove('active');
+    current -= 1;
+});
+
+prevBtnFourth.addEventListener('click', () => {
+    slidePage.style.marginLeft = "-50%";
+    bullet[current - 2].classList.remove('active');
+    progressText[current - 2].classList.remove('active');
+    progressCheck[current - 2].classList.remove('active');
+    current -= 1;
+});
+
+// botões de previous até aqui
+
+// botão de submit aqui
+submitBtn.addEventListener('click', () => {
+    bullet[current - 1].classList.add('active');
+    progressText[current - 1].classList.add('active');
+    progressCheck[current - 1].classList.add('active');
+    current += 1;
+    setTimeout(() => {
+        alert("You're successfully signed up!");
+        location.reload();
+    }, 800)
+});
+
